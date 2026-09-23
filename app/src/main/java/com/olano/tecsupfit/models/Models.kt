281170dd93eb14826f6665e8224725cc29432859
Brayan Olano <brayanolano21@icloud.com>
2026-09-23 11:25:51 -0500

    creacion de modelos de datos Doctor, Cita y Usuario

--- app/src/main/java/com/olano/tecsupfit/models/Models.kt
+++ app/src/main/java/com/olano/tecsupfit/models/Models.kt
@@ -1,3 +1,24 @@
-package com.olano.tecsupfit.models
+package com.olano.navlab.model
 
+data class Doctor(
+    val id: Int,
+    val nombre: String,
+    val especialidad: String,
+    val experiencia: String,
+    val calificacion: Double,
+    val resenas: Int,
+    val biografia: String
+)
 
+data class Cita(
+    val doctorNombre: String,
+    val fecha: String,
+    val hora: String,
+    val estado: String // "Confirmada" o "Completada"
+)
+
+data class Usuario(
+    val nombre: String = "Brayan Olano",
+    val correo: String = "brayan.olano@tecsup.edu.pe",
+    val rol: String = "Paciente Premium"
+)
